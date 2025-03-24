888179a658bec30688349440f508ebf784256288
TilsCordeiro <tilscordeiro@gmail.com>
2025-03-24 10:22:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 #http://127.0.0.1:8000/ para acessar no navegador
-@app.get("/")
+@app.get("/helloword")
 async def root():
     return {"message": "Hello World"}
 
 #http://127.0.0.1:8000/teste1 para acessar no navegador
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo!"}
